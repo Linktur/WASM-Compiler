843d199db8eda2aa75cea5fc2bca33e2ac979777
Vsevolod <seva.nazmudinov@gmail.com>
2025-09-10 13:07:21 +0300

    Lexer

---
+++ Compilers-project/Compilers-project/Lexer/Interfaces/ILexer.cs
@@ -0,0 +1,8 @@
+﻿namespace Compilers_project.Lexer.Interfaces;
+
+public interface ILexer
+{
+    Token NextToken(); 
+    Token Peek(int k = 1);     
+    void  Reset(int position = 0);
+}
---
+++ Compilers-project/Compilers-project/Lexer/TokenType.cs
@@ -0,0 +1,20 @@
+﻿namespace Compilers_project.Lexer;
+
+public enum TokenType
+{
+    // хз,как описать спецфигня
+    Eof, Error, NewLine,
+
+    // идентификаторы/литералы
+    Identifier, IntegerLiteral, RealLiteral, BooleanLiteral,
+
+    // ключевые слова
+    Var, Type, Record, Array, Routine, Is, End,
+    If, Then, Else, While, For, Reverse, Loop, Return, Print,
+    And, Or, Xor, Not,
+
+    // знаки/операторы
+    LParen, RParen, LBracket, RBracket, Comma, Colon, Semicolon, Dot,
+    DotDot, Assign, Arrow, Plus, Minus, Star, Slash, Percent,
+    Less, LessEqual, Greater, GreaterEqual, Equal, NotEqual
+}
